3da52cc127dd61846767e86d796e4319a2e621b2
Jake Declock <derilojake2528@gmail.com>
2026-05-17 09:47:25 +0800

    changeee

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,9 @@ import Register from "./pages/Register";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/register" element={<Register />} />
+      <Route path="/" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+
       <Route
         path="/dashboard"
         element={

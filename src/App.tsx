1cd4af0f5ac5555c8213d67de624ae049497df3d
Jake Declock <derilojake2528@gmail.com>
2026-05-17 10:00:28 +0800

    fixx desit

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,8 @@ import Register from "./pages/Register";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Register />} />
-      <Route path="/login" element={<Login />} />
+      <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Register />} />
 
       <Route
         path="/dashboard"
